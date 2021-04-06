f62d8e96392665fa2bef86a6c590aa3e5aeb0e42
liambrekke <liam.brekke@gmail.com>
2021-04-05 19:15:52 -0500

    added table

--- ufo-lv-1/static/js/app.js
+++ ufo-lv-1/static/js/app.js
@@ -9,3 +9,17 @@ var tbody = d3.select("tbody");
 
 data.forEach(function(aliens) {
   console.log(aliens) });
+
+// Step 2:  Use d3 to append one table row `tr` for each alien sighting  object
+
+data.forEach(function(aliens) {
+  console.log(aliens);
+  var row = tbody.append("tr");
+
+  Object.entries(aliens).forEach(function([key, value]) {
+    // console.log(key, value);
+        // Append a cell to the row for each value
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
