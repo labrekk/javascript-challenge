e809d6f09b97a1f7701d839a0b4d932220b1ee57
liambrekke <liam.brekke@gmail.com>
2021-04-05 19:06:41 -0500

    aliens function in app.js

--- ufo-lv-1/static/js/app.js
+++ ufo-lv-1/static/js/app.js
@@ -4,6 +4,8 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the sighting data from data.js
-console.log(data);
+// Console.log the alien sighting data from data.js
+// console.log(data);
 
+data.forEach(function(aliens) {
+  console.log(aliens) });
